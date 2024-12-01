24232d63aa5106596ffeda4324a34b071498ea0d
kdennewith <kdennewith@gmail.com>
2024-12-01 11:32:55 -0800

    Added comments

--- app/src/main/java/com/example/recipebookgui/RecipeListener.java
+++ app/src/main/java/com/example/recipebookgui/RecipeListener.java
@@ -1,6 +1,8 @@
 package com.example.recipebookgui;
 
 public interface RecipeListener {
+
+    // Want to use this to have a onClick to go to Recipe page when selecting a Recipe on the HomePage
     void onRecipeAction(Boolean isSelected);
 
 }
